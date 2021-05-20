2946527834227c48f21caa30a98ab238460eede2
Amarender Tangella <ATangella@morneaushepell.com>
2021-05-19 20:46:47 -0400

    first

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var particlesJS: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    particlesJS.load(
+      "particles-js",
+      "../../assets/config/particlesjs-config.json",
+      null
+    );
+  }
+
+}
